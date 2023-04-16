b8dc93154e16d6e4f7550703396e189835787ab5
Inho Choi <inhoru126@gmail.com>
2023-04-17 00:02:13 +0900

    Music

---
+++ 최인호/src/com/bs/practice/list/music/model/vo/Music.java
@@ -0,0 +1,66 @@
+package com.bs.practice.list.music.model.vo;
+
+import java.util.Objects;
+
+import com.bs.practice.list.library.model.vo.Book;
+
+public class Music implements Comparable<Music>{
+	private String title;
+	private String singer;
+	
+	public Music() {}
+	
+	public Music(String title, String singer) {
+		super();
+		this.title = title;
+		this.singer = singer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	@Override
+	public String toString() {
+		return singer + "-" + title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(singer, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Music other = (Music) obj;
+		return Objects.equals(singer, other.singer) && Objects.equals(title, other.title);
+	}
+	
+
+	
+	@Override
+	public int compareTo(Music o) {
+		return o.getSinger().compareTo(this.getSinger());
+	}
+	
+	
+	
+}
---
+++ 최인호/src/com/bs/practice/list/music/run/Run.java
@@ -0,0 +1,12 @@
+package com.bs.practioce.list.music.run;
+
+import com.bs.practice.list.view.MusicView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new MusicView().mainMenu();	
+		
+	}
+
+}
